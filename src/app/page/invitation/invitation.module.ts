aea0a9d6c70259218be4780a8e1105a38d4a0d3f
v_mmingyu <v_mmingyu@tencent.com>
2019-07-11 16:36:32 +0800

    添加前端生成二维码功能

--- src/app/page/invitation/invitation.module.ts
+++ src/app/page/invitation/invitation.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
+import { QRCodeModule } from 'angular2-qrcode';
 
 import { InvitationPage } from './invitation.page';
 
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    QRCodeModule
   ],
   declarations: [InvitationPage]
 })
